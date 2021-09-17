8b2b340123762b89aa55c721abeca1821f1ecd41
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-17 12:37:43 -0700

    post endpoint commit

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const JWT_SECRET = "hello"
+const {JWT_SECRET} = require('../../config') 
 module.exports = function (user) {
   const payload = {
     subject: user.id,
